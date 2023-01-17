58632ccf5bc5028eb3b82e95eff57982ba3ff278
Rahulk1999 <k.rahul@tftus.com>
2023-01-17 15:05:59 +0530

    navebar using loop

--- src/app/NavSection/dashboard/dashboard.component.ts
+++ src/app/NavSection/dashboard/dashboard.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  navs = [
+    { url: 'publisher-rule', content: "Publisher Rule"},
+    { url: 'feed-manager', content: "Feed Manager" },
+    { url: 'referrer-manager', content: "Referrer Manager" },
+    { url: 'search-pixel-manager', content: "Search Pixel Manager" },
+    { url: 'country-manager', content: "country manager" },
+    { url: 'domain-manager', content: "Domain Manager" }
+  ]
 }
